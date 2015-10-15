148c996b6fac55647471f9e89ce68b2208b79ea6
Yvonne Peng <ypeng.tw@gmail.com>
2015-10-15 16:53:43 -0700

    Add error dialog

--- app/src/main/java/com/example/guest/myweather/MainActivity.java
+++ app/src/main/java/com/example/guest/myweather/MainActivity.java
@@ -2,36 +2,63 @@ package com.example.guest.myweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import java.io.IOException;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import javax.security.auth.callback.Callback;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    String apiKey = "12cf5c054101992f485f24eceeb58e8e";
+    double latitude = 45.5311331;
+    double longitude = -122.6824974;
+    String forecastURL = "https://api.forecast.io/forecast/"+ apiKey +"/"+ latitude +","+ longitude;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    OkHttpClient client = new OkHttpClient();
+    Request request = new Request.Builder()
+            .url(forecastURL)
+            .build();
+    Call call = client.newCall(request);
+    call.enqueue(new Callback() {
+        @Override
+        public void onFailure (Request request, IOException e){
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public void onResponse (Response response)throws IOException {
+            try {
+                // Response response = call.execute();
+                Log.v(TAG, response.body().string());
+                if (response.isSuccessful()) {
+
+                } else {
+                    alertUserAboutError();
+                }
+            } catch (IOException e) {
+                // e.printStackTrace();
+                Log.e(TAG, "Exception caught: ", e);
+            }
+        }
+
+        });
+        Log.d(TAG, "Main UI code is running!");
     }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
+    }
+
 }
