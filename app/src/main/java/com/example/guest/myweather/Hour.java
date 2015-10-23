0d635d47b8343e06bf29a9c77cdb2f5dadc77e34
YHop <ypeng.tw@gmail.com>
2015-10-23 10:57:55 -0700

    Add Hour and Day class

---
+++ app/src/main/java/com/example/guest/myweather/Hour.java
@@ -0,0 +1,50 @@
+package com.example.guest.myweather;
+
+/**
+ * Created by YHoP on 10/23/15.
+ */
+public class Hour {
+    private long mTime;
+    private String mSummary, mIcon, mTimezone;
+    private double mTemperature;
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public String getTimezone() {
+        return mTimezone;
+    }
+
+    public void setTimezone(String timezone) {
+        mTimezone = timezone;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+}
