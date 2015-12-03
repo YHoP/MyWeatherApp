92671b315e1b457d28579763d9fe7ca79fd8ed2e
YHop <ypeng.tw@gmail.com>
2015-12-02 17:31:09 -0800

    Modify Hour class, implements Parcelable

--- app/src/main/java/com/example/yhop/myweather/weather/Hour.java
+++ app/src/main/java/com/example/yhop/myweather/weather/Hour.java
@@ -1,9 +1,39 @@
 package com.example.yhop.myweather.weather;
 
-public class Hour {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Hour implements Parcelable {
+    public static final Creator<Hour> CREATOR = new Creator<Hour>() {
+        @Override
+        public Hour createFromParcel(Parcel source) {
+            return new Hour(source);
+        }
+
+        @Override
+        public Hour[] newArray(int size) {
+            return new Hour[size];
+        }
+    };
     private long mTime;
-    private String mSummary, mIcon, mTimezone;
+    private String mSummary;
     private double mTemperature;
+    private String mIcon;
+    private String mTimezone;
+
+    public Hour() {
+    }
+
+    private Hour(Parcel in) {
+        mTime = in.readLong();
+        mTemperature = in.readDouble();
+        mSummary = in.readString();
+        mIcon = in.readString();
+        mTimezone = in.readString();
+    }
 
     public long getTime() {
         return mTime;
@@ -21,12 +51,24 @@ public class Hour {
         mSummary = summary;
     }
 
+    public int getTemperature() {
+        return (int) Math.round(mTemperature);
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
     public String getIcon() {
         return mIcon;
     }
 
     public void setIcon(String icon) {
         mIcon = icon;
+    }
+
+    public int getIconId() {
+        return Forecast.getIconId(mIcon);
     }
 
     public String getTimezone() {
@@ -37,11 +79,23 @@ public class Hour {
         mTimezone = timezone;
     }
 
-    public double getTemperature() {
-        return mTemperature;
+    public String getHour() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h a");
+        Date date = new Date(mTime * 1000);
+        return formatter.format(date);
     }
 
-    public void setTemperature(double temperature) {
-        mTemperature = temperature;
+    @Override
+    public int describeContents() {
+        return 0; // ignore
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mTime);
+        dest.writeDouble(mTemperature);
+        dest.writeString(mSummary);
+        dest.writeString(mIcon);
+        dest.writeString(mTimezone);
     }
 }
