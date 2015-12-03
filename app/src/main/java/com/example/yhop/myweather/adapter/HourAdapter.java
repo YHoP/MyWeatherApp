4f6cb571c7c4fed71067f8fb2cc0c616c3fbd2fd
YHop <ypeng.tw@gmail.com>
2015-12-02 17:31:55 -0800

    Modify HourAdapter to display forecast info

--- app/src/main/java/com/example/yhop/myweather/adapter/HourAdapter.java
+++ app/src/main/java/com/example/yhop/myweather/adapter/HourAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.yhop.myweather.R;
 import com.example.yhop.myweather.weather.Hour;
@@ -49,12 +50,23 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
             mTimeLabel = (TextView)itemView.findViewById(R.id.timeLabel);
             mSummaryLabel = (TextView) itemView.findViewById(R.id.summaryLabel);
             mTemperatureLabel = (TextView) itemView.findViewById(R.id.temperatureLabel);
+            itemView.setOnClickListener(this);
+        }
 
+        public void bindHour(Hour hour) {
+            mTimeLabel.setText(hour.getHour());
+            mSummaryLabel.setText(hour.getSummary());
+            mTemperatureLabel.setText(hour.getTemperature() + "");
+            mIconImageView.setImageResource(hour.getIconId());
         }
 
         @Override
         public void onClick(View v) {
-
+            String time = mTimeLabel.getText().toString();
+            String temperature = mTemperatureLabel.getText().toString();
+            String summary = mSummaryLabel.getText().toString();
+            String message = String.format("At %s it will be %s and %s", time, temperature, summary);
+            Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();
         }
     }
 }
