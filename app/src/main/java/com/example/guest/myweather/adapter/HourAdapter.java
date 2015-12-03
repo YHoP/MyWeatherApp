a02c3c9d7c1872058595ec20e72b54a65f765072
YHop <ypeng.tw@gmail.com>
2015-12-02 16:52:16 -0800

    Merge changes

---
+++ app/src/main/java/com/example/guest/myweather/adapter/HourAdapter.java
@@ -0,0 +1,60 @@
+package com.example.guest.myweather.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.guest.myweather.R;
+import com.example.guest.myweather.weather.Hour;
+
+public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder>{
+
+    private Hour[] mHours;
+    private Context mContext;
+
+    public HourAdapter(Context context, Hour[] hours){
+        mContext = context;
+        mHours = hours;
+    }
+
+
+    @Override
+    public HourAdapter.HourViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(HourAdapter.HourViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mHours.length;
+    }
+
+    public class HourViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public TextView mTimeLabel;
+        public TextView mSummaryLabel;
+        public TextView mTemperatureLabel;
+        public ImageView mIconImageView;
+
+        public HourViewHolder(View itemView){
+            super(itemView);
+
+            mTimeLabel = (TextView)itemView.findViewById(R.id.timeLabel);
+            mSummaryLabel = (TextView) itemView.findViewById(R.id.summaryLabel);
+            mTemperatureLabel = (TextView) itemView.findViewById(R.id.temperatureLabel);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/guest/myweather/ui/HourlyForecastActivity.java
@@ -0,0 +1,42 @@
+package com.example.guest.myweather.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.guest.myweather.R;
+import com.example.guest.myweather.adapter.HourAdapter;
+import com.example.guest.myweather.weather.Hour;
+
+import java.util.Arrays;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class HourlyForecastActivity extends ActionBarActivity {
+
+    private Hour[] mHours;
+    @Bind(R.id.reyclerView) RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
+        mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
+        HourAdapter adapter = new HourAdapter(this, mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
+    }
+}
