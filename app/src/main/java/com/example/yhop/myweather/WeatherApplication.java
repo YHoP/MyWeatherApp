b01394ab8224ac7d5335d3a7781e9823338f748c
YHop <ypeng.tw@gmail.com>
2016-02-05 13:55:35 -0800

    Add api url to the application

--- app/src/main/java/com/example/yhop/myweather/WeatherApplication.java
+++ app/src/main/java/com/example/yhop/myweather/WeatherApplication.java
@@ -4,10 +4,15 @@ import android.app.Application;
 
 public class WeatherApplication extends Application {
     private static WeatherApplication mApplication;
+    private String api_url;
     private String api_key;
 
     public static WeatherApplication getInstance() {
         return mApplication;
+    }
+
+    public String getApiUrl() {
+        return api_url;
     }
 
     public String getApiKey() {
@@ -18,6 +23,7 @@ public class WeatherApplication extends Application {
     public void onCreate() {
         super.onCreate();
         mApplication = this;
+        api_url = "https://api.forecast.io/forecast/";
         api_key = getResources().getString(R.string.api_key);
     }
 
